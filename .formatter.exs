96ff63f4368339779057f381f6c3b7befcfd7817
Loc Nguyen <278194+locsic@users.noreply.github.com>
2026-04-02 16:56:07 -0700

    new new new

--- .formatter.exs
+++ .formatter.exs
@@ -19,7 +19,6 @@
     :ecto_sql,
     :phoenix
   ],
-  subdirectories: ["priv/*/migrations"],
   plugins: [Spark.Formatter, Phoenix.LiveView.HTMLFormatter],
   inputs: ["*.{heex,ex,exs}", "{config,lib,test}/**/*.{heex,ex,exs}", "priv/*/seeds.exs"]
 ]
